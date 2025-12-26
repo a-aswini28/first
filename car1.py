ff7150f7a2519d5445ed1d55c770c4ff69ebc697
a-aswini28 <aravaaswinireddyas@gmail.com>
2025-12-26 15:07:00 +0530

    Create car1.py

---
+++ car1.py
@@ -0,0 +1,14 @@
+Class Car:
+def__init__(self,name,price,colour):
+  self.n=name
+  self.p=price
+  self.c=colour
+c1=car("swity",34567,"white")
+c2=car("toyota",56437,"black")
+print(c1.name)
+print(c1.price)
+print(c1.colour)
+c2=car("toyota",8764532,"black")
+print(c2.name)
+print(c2.price)
+print(c3.black)
