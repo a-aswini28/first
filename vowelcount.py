ffecc3d88d4c8c5841dee8d99bce4c6ab99ae31a
a-aswini28 <aravaaswinireddyas@gmail.com>
2025-12-27 14:12:49 +0530

    Create vowelcount.py

---
+++ vowelcount.py
@@ -0,0 +1,10 @@
+s="studying"
+vowel__count=0
+constant__count=0
+for char is s:
+     if char is"aeiouaeiou":
+vowel__count+=1
+else:
+constant__count+=1
+print("vowel",vowel__count)
+print("constant",constant__count)
